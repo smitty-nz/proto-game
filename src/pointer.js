d6bb66afa73a9fccf9f0cdabfed0f6580bfe9dd0
smith <smitty-nz@protonmail.com>
2018-11-15 12:50:18 +1300

    pointer update
    added events to pointer:
    +wheel <direction> <delta>
    +press <button> <position>
    +release <button> <position>

--- src/pointer.js
+++ src/pointer.js
@@ -9,15 +9,23 @@ class Pointer extends Events.EventEmitter {
         this.pixel = this.position;
         this.game = game;
         this.buttons = [];
+        this.buttonsAlias = [ 'left', 'middle', 'right' ]
+        this.BUTTON_LEFT = 0;
+        this.BUTTON_RIGHT = 2;
+        this.BUTTON_MIDDLE = 1;
         this.__register( game );
     }
-    get left(){ return this.buttons[ 0 ] };
-    get right(){ return this.buttons[ 2 ] };
-    get middle(){ return this.buttons[ 1 ] };
+    get left(){ return this.buttons[ 0 ]; }
+    get right(){ return this.buttons[ 2 ]; }
+    get middle(){ return this.buttons[ 1 ]; }
+    isButtonDown( button ){ 
+        return !!this.buttons[ button ]; 
+    }
     __register( game ){
         game.on( 'mouseup', event => { this.__setButtonUp( event ) } );
         game.on( 'mousedown', event => { this.__setButtonDown( event ) } );
         game.on( 'mousemove', event => { this.__setPosition( event ) } );
+        game.on( 'mousewheel', event =>{ this.__setWheel( event ) } );
     }
     __setPosition( event ){
         let target = this.game.outputElement;
@@ -35,15 +43,22 @@ class Pointer extends Events.EventEmitter {
     }
     __setButtonDown( event ){
         this.buttons[ event.button ] = true;
-        if( event.button == 0 ) this.emit( 'pressLeft', this.position );     // todo: streamline
-        if( event.button == 2 ) this.emit( 'pressRight', this.position );
-        if( event.button == 1 ) this.emit( 'pressMiddle', this.position );
+        this.emit( 'press', this.buttonsAlias[ event.button ], this.position );
+        if( event.button == this.BUTTON_LEFT ) this.emit( 'pressLeft', this.position );     // todo: streamline
+        if( event.button == this.BUTTON_RIGHT ) this.emit( 'pressRight', this.position );
+        if( event.button == this.BUTTON_MIDDLE ) this.emit( 'pressMiddle', this.position );
     }
     __setButtonUp( event ){
         this.buttons[ event.button ] = false;
-        if( event.button == 0 ) this.emit( 'releaseLeft', this.position );       // todo: streamline
-        if( event.button == 2 ) this.emit( 'releaseRight', this.position );
-        if( event.button == 1 ) this.emit( 'releaseMiddle', this.position );
+        this.emit( 'release', this.buttonsAlias[ event.button ], this.position );
+        if( event.button == this.BUTTON_LEFT ) this.emit( 'releaseLeft', this.position );       // todo: streamline
+        if( event.button == this.BUTTON_RIGHT ) this.emit( 'releaseRight', this.position );
+        if( event.button == this.BUTTON_MIDDLE ) this.emit( 'releaseMiddle', this.position );
+    }
+    __setWheel( event ){
+        let delta = event.deltaY;
+        if( delta < 0 ) this.emit( 'wheel', 'up', delta );
+        if( delta > 0 ) this.emit( 'wheel', 'down', delta );
     }
 }
 
